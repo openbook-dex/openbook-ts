b6dd932944b1f4ee976d071ab5edc20bd0ac4276
Binye Barwe <binyebarwe@gmail.com>
2023-05-15 16:13:58 +0200

    update versions for release v0.0.7

--- publish.sh
+++ publish.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cd packages/associated-token
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../borsh
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../tokens
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../openbook
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../common
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../token
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
 cd ../swap
-yarn install && yarn publish $@
+yarn install && yarn publish $@ --no-git-tag-version
